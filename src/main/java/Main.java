0caeaa61d6079a9952487bb545917f6cda967f50
Alirezanbz <sanabavizadeh@actum.de>
2023-07-06 11:51:49 +0200

    feat: frontend

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,14 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        MySQLStatements statements = new MySQLStatements();
+
         HomePageFrame homePageFrame = new HomePageFrame();
         homePageFrame.openHomePage();
-        Queries queries = new Queries();
+
 
 
         //System.out.println(queries.selectQuery("nachname", "kunden", "where kunden.kdnr = 1001"));
 
-        System.out.println(statements.getRezepts());
+
     }
 }
